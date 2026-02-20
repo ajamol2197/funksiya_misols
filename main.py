b3408b051e9cca261526af34d47b9ce4e5fded9d
ajamol2197 <ajamol2197@gmail.com>
2026-02-20 16:29:17 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,62 @@
+# 1
+def ism(a, b):
+    return a+b
+
+
+print(ism(13, 5))
+
+# 2
+def raqam(a):
+    return a*a
+
+
+print(f"parametrni kvadrati {raqam(3)}")
+
+# 3
+def ism(a):
+    return len(a)
+
+
+print(f"ism uzunligi: {ism("aslbek")}")
+
+# 4
+def rr(a, b):
+    return max(a + b)
+
+
+print(f"maksimal raqami: {rr(12, 4)}")
+
+# 5
+def matn(a):
+    return a
+
+
+print(f"sizning matninggiz: {"matnbvek"}")
+
+# 6
+def raqam(a, b):
+    return a-b
+
+
+print(f"matnning farqi: {raqam(13, 6)}")
+
+# 7
+def raq(a, b):
+    return a*b
+
+
+print(f" matnning kopaytmasi: {raq(2, 4)}")
+
+# 8
+def raq(a, b):
+    return a/b
+
+
+print(f" matnning bolinmasi: {raq(4, 2)}")
+
+# 10
+def ii(a, b="salom "):
+    return b+a
+
+
+print(ii("asl"))
